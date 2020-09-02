2d3c5b2d2ebd8d1e2d452f683580b448f10e06fb
kimxwan0319 <kimxwan0319@naver.com>
2020-09-02 23:01:00 +0900

    [Upload] Code

---
+++ SeaFood_iOS/SeaFood_iOS/myPageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  myPageViewController.swift
+//  SeaFood_iOS
+//
+//  Created by 김수완 on 2020/09/02.
+//  Copyright © 2020 김수완. All rights reserved.
+//
+
+import UIKit
+
+class myPageViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
